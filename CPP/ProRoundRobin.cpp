4bd5a29598b90887d5237b403a54ca95abe887b9
suscoder11 <119058788+suscoder11@users.noreply.github.com>
2023-04-17 03:34:30 +0200

    SchdeduleAlgo() implemented in RR//inctimer() and dectimer() both are modified

--- CPP/ProRoundRobin.cpp
+++ CPP/ProRoundRobin.cpp
@@ -24,15 +24,51 @@ int ProRoundRobin::gettimer() const
 
 
 bool ProRoundRobin::ScheduleAlgo()
-{}
-
-void ProRoundRobin::inctimer(int timestep)
 {
 
-	inctimer(p->getCpuTime());
+
+	if (RUNLIST == nullptr) {
+
+		RUNLIST = Plist.dequeue();
+
+		dectimer(RUNLIST);
+
+	}
+
+	else
+		if (counter <= timeslice) {
+
+
+			RUNLIST->decremainingtime();
+
+			counter++;
+		}
+
+		else
+
+		{
+			Plist.Queue(RUNLIST);
+			inctimer(RUNLIST);
+			RUNLIST = nullptr;
+
+		}
+
+
+
 
 }
 
-void ProRoundRobin::dectimer(int timestep)
-{}
+void ProRoundRobin::inctimer(Process* p)
+{
+
+	timer+=(p->getremainingtime());
+
+}
+
+void ProRoundRobin::dectimer(Process *p)
+{
+
+	timer -= (p->getremainingtime());
+
+}
 
